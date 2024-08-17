6b3d83499d7770baf04c352e1a839d434e0dd71c
KaueAnjos <kaueramada06@gmail.com>
2024-08-17 16:46:30 -0300

    Configurações e estruturra básica das páginas e arquivos.

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// path site
+define('INCLUDE_PATH', 'http://localhost/Employify/');
+
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'employify-bd');
+
+// require 'connection.php';
+?>
