01dcb1ea3421692dc6dfd08777659dcb024de02f
KaueAnjos <kaueramada06@gmail.com>
2024-09-24 20:59:24 -0300

    Atualização das funcionalidades da navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,16 @@ window.addEventListener("load", function () {
     loadingDots.classList.add("hidden");
 });
 
+
+// Navbar Transition
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("body nav");
+    if (window.scrollY > 0) {
+        header.style.background = "radial-gradient(circle, rgba(9,31,47,1) 0%, rgba(9,31,47,1) 66%, rgba(17,58,83,1) 100%)";
+        header.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
+    } else {
+        header.style.background = "transparent";
+        header.style.boxShadow = "none";
+    }
+});
