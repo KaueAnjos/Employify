3aaafef02fb7e6911a877f405a2e61fe1adcc1a5
KaueAnjos <kaueramada06@gmail.com>
2024-11-22 11:37:00 -0300

    Adicionando tela 404

--- 404.php
+++ 404.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>assets/css/404-style.css" />
     <title>404 Page not Found</title>
 </head>
+
 <body>
-    
+    <main>
+        <section>
+            <h1>404 Error - Page not Found</h1>
+        </section>
+    </main>
 </body>
+
 </html>
