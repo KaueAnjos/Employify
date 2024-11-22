7598fb4a0d3cee83dbddf39c317e6abe41eea9ee
KaueAnjos <kaueramada06@gmail.com>
2024-11-22 09:33:11 -0300

    Correção de erros do menu

--- feed/courses.php
+++ feed/courses.php
@@ -1,10 +1,10 @@
 <head>
     <title>Como criar Cards e esquecer de vez o Panel e o Thumbnail - Criando um site com Bootstrap 4 - Aula 06</title>
-    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>assets/css/home-style.css" />
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>assets/css/courses-style.css" />
 </head>
 
 <main>
     <section class="main-menu">
-
+        <img src="<?php echo INCLUDE_PATH; ?>assets/img/Thumbnail.jfif">
     </section>
 </main>
