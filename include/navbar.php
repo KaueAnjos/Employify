46a13b11f31c4aea228bf00a9f563a98e730434a
KaueAnjos <kaueramada06@gmail.com>
2024-11-27 07:59:19 -0300

    Adcionando o perfil do usuário

--- include/navbar.php
+++ include/navbar.php
@@ -27,7 +27,7 @@ session_start();  // Iniciar a sessão para acessar as variáveis de sessão
         </li>
 
         <li class="user-profile" id="user-profile">
-            <?php if (isset($_SESSION['user_id'])): ?>
+            <?php if (isset($_SESSION['user_id'])) : ?>
                 <div class="user-info">
                     <span>Olá, <?php echo $_SESSION['username']; ?></span>
                 </div>
@@ -55,7 +55,7 @@ session_start();  // Iniciar a sessão para acessar as variáveis de sessão
                             </a>
                         </li>
                         <li>
-                            <a class="dropdown-item" href="#">
+                            <a class="dropdown-item" href="<?php echo INCLUDE_PATH_USERS ?>profile">
                                 <i class="fa fa-sign-in"></i>
                                 <p>Acessar Perfil</p>
                             </a>
@@ -63,7 +63,7 @@ session_start();  // Iniciar a sessão para acessar as variáveis de sessão
                     </ul>
                 </div>
                 <a href="<?php echo INCLUDE_PATH_AUTH ?>logout"><button>Sair</button></a>
-            <?php else: ?>
+            <?php else : ?>
                 <div class="register-user">
                     <a href="<?php echo INCLUDE_PATH_AUTH ?>login"><button>Entrar</button></a>
                     <a href="<?php echo INCLUDE_PATH_AUTH ?>signup"><button>Registrar-se</button></a>
@@ -73,7 +73,28 @@ session_start();  // Iniciar a sessão para acessar as variáveis de sessão
                         <i class="fa-solid fa-user"></i>
                     </button>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="<?php echo INCLUDE_PATH_AUTH ?>login"><i class="fa fa-sign-in" aria-hidden="true"></i>Entrar</a></li>
+                        <li>
+                            <a class="dropdown-item" href="<?php echo INCLUDE_PATH_AUTH ?>login"><i class="fa fa-sign-in" aria-hidden="true"></i>Entrar</a>
+                        </li>                        
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <div>
+                                    <i class="fa-solid fa-moon"></i>
+                                    <p>Tema Escuro</p>
+                                </div>
+                                <label class="switch">
+                                    <input type="checkbox" checked>
+                                    <span class="slider"></span>
+                                </label>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fa-solid fa-globe"></i>
+                                <p>Idioma</p>
+                            </a>
+                        </li>
+
                     </ul>
                 </div>
             <?php endif; ?>
--- users/profile.php
+++ users/profile.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php
+require_once '../config/config.php';
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employify | Perfil</title>
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH_USERS; ?>assets/css/profile-style.css" />
 </head>
+
 <body>
-    
+    <main>
+        <section class="profile">
+            <div class="profile-container">
+                <div class="user-data">
+                    <div><img src="" alt="user-photo"></div>
+                    <h2>Name User</h2>
+                    <h3>Sobre Mim</h3>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+
+                <div class="buttons">
+                    <button>Employify IA</button>
+                    <button>Editar Perfil</button>
+                    <button>Seus Cursos</button>
+                    <button>Histórico de Cursos</button>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
-</html>
