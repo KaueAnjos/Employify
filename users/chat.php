6c75ce74cb3f29c9c46919d9b5ca1260d4d93b4c
KaueAnjos <kaueramada06@gmail.com>
2024-12-02 12:22:23 -0300

    Chat IA

---
+++ users/chat.php
@@ -0,0 +1,37 @@
+<head>
+    <title>Employify | Employify IA</title>
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH_USERS; ?>assets/css/chat-style.css" />
+</head>
+
+<main>
+    <section>
+        <h1>EMPLOYIFY IA</h1>
+        <div class="chat-container">
+
+            <div class="container">
+                <div class="user-text-container text-container">Me chamo Sara, tenho 32 anos, trabalho como operadora de caixa e o meu serviço está sendo substituído por IA e tecnologia.. o que posso cursar na área de TI para ingressar no mercado de trabalho atual e me preparar para o futuro ? Qual área me daria bem e por que ? obs.: gostaria juntar minhas competências como operadora de caixa na profissão. </div>
+                <img src="<?php echo INCLUDE_PATH; ?>assets/img/default.jpg" alt="Imagem Usuário">
+            </div>
+
+            <div class="container">
+                
+                <img src="<?php echo INCLUDE_PATH; ?>assets/img/LogoEmployify.png" alt="Imagem IA">
+                <div class="ia-text-container text-container">
+                    Sara, considerando sua experiência como operadora de caixa, você já possui habilidades valiosas, como atendimento ao cliente, organização, gestão de transações e agilidade com sistemas de vendas. Na área de TI, você pode aproveitar essas competências em algumas áreas promissoras:
+
+                    1. Análise de Dados (Data Analytics)
+
+                    Por que combina com você?
+                    Trabalhar como operadora de caixa envolve lidar com dados de vendas, comportamento de clientes e inventário. Essas experiências podem ser úteis para analisar padrões e tomar decisões baseadas em dados.
+
+                    O que você faria?
+                    Interpretar dados de vendas para melhorar estratégias de marketing, prever tendências e otimizar operações de negócios.
+
+                    Curso recomendado:
+                    Introdução à Análise de Dados (Employify).
+                    Ferramentas como Excel avançado, Power BI e SQL.
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
